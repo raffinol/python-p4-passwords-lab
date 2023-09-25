9be9500ec59cc57e666a9fb8a2f2befe550b6a16
Rafael Finol <rafaelfinol88@gmail.com>
2023-09-24 23:30:51 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -6,38 +6,55 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User
 
-class ClearSession(Resource):
 
+class ClearSession(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session["page_views"] = None
+        session["user_id"] = None
 
         return {}, 204
 
+
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
+        user = User(username=json["username"])
+        user.password_hash = json["password"]
         db.session.add(user)
         db.session.commit()
+        session["user_id"] = user.id
         return user.to_dict(), 201
 
+
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get("user_id"):
+            user = User.query.filter(User.id == session["user_id"]).first()
+            return user.to_dict(), 200
+        return {}, 204
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()["username"]
+        ).first()
+
+        if user.authenticate(request.get_json()["password"]):
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session["user_id"] = None
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
-api.add_resource(Signup, '/signup', endpoint='signup')
 
-if __name__ == '__main__':
+api.add_resource(ClearSession, "/clear", endpoint="clear")
+api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(CheckSession, "/check_session", endpoint="check_session")
+api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(Logout, "/logout", endpoint="logout")
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
